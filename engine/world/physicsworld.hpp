8d80121690c7da770ead4170c1943e22491b7975
Satvik-Singh192 <satviksingh192@gmail.com>
2026-03-20 18:17:12 +0530

    feat: warm start implemented

--- engine/world/physicsworld.hpp
+++ engine/world/physicsworld.hpp
@@ -8,6 +8,7 @@ class PhysicsWorld {
 private: 
 	std::vector<Rigidbody> bodies;
 	std::vector<Contact>contacts;
+	std::vector<Contact>prev_contacts;
 	Vec3 gravity;
 	BodyID next_body_id;
 public: 
@@ -22,6 +23,8 @@ public:
 
 	void clear_contacts();
 	void generate_contacts();
+	void match_contacts();
 	void solve_contacts(); //solves for vel
 	void solve_position();//tis solves for position overlap
+	void warm_start_contacts();
 };
