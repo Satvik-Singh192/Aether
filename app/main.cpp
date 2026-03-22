981dd5f914958d7ba895468428a914eb0272a2c6
PresenceOP-Coder <shreyasprajapati1230@gmail.com>
2026-03-22 15:59:06 +0530

    feat: added ramp manifold system

--- app/main.cpp
+++ app/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     PhysicsWorld world;
 
-    const TestCase active_case = TestCase::SphereRamp;
+    const TestCase active_case = TestCase::BoxRamp;
     LoadSingleTestScenario(world, active_case);
 
     CreateWindow(world);
