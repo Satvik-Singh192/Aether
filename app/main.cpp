26a0fd8d38f709175ba6f82f902f3451fc057760
Satvik-Singh192 <satviksingh192@gmail.com>
2026-03-22 16:34:31 +0530

    chore: resturctured sphere ramp manifold generation function

--- app/main.cpp
+++ app/main.cpp
@@ -7,7 +7,7 @@ int main()
 {
     PhysicsWorld world;
 
-    const TestCase active_case = TestCase::SphereBox;
+    const TestCase active_case = TestCase::BoxSphereRamp;
     LoadSingleTestScenario(world, active_case);
 
     CreateWindow(world);
--- engine/world/physicsworld.cpp
+++ engine/world/physicsworld.cpp
@@ -207,15 +207,8 @@ void PhysicsWorld::generate_contacts()
 			else if (a.collider->type == ShapeType::Sphere &&
 					 b.collider->type == ShapeType::Ramp)
 			{
-				if (buildSphereRampContact(a, b, c))
+				if (buildRampSphereManifold(b,a,m))
 				{
-					m.contacts[0] = c;
-					m.contact_count = 1;
-					m.normal = c.normal;
-					m.a = c.a;
-					m.b = c.b;
-					m.a_id = c.a->id;
-					m.b_id = c.b->id;
 					manifolds.push_back(m);
 				}
 			}
