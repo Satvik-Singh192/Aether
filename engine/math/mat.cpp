8eee4dc7d65cd3bda75efa565a3b039910b5daa5
urastogi2048 <utkarshrastogi2048@gmail.com>
2026-03-22 17:14:51 +0530

    initiated2.0

--- engine/math/mat.cpp
+++ engine/math/mat.cpp
@@ -14,5 +14,31 @@ Mat3 Mat3::identity(){
     return mat;
 }
 Mat3 Mat3::transpose() const {
-    
+    Mat3 result;
+    for(int i=0; i<3; ++i){
+        for(int j=0;j<3;++j){
+            result.m[i][j]=m[j][i];
+        }
+
+    }
+    return result;
+}
+Vec3 Mat3::operator*(const Vec3& v) const {
+    return Vec3(
+        m[0][0]*v.x+m[0][1]*v.y + m[0][2]*v.z,
+        m[1][0]*v.x + m[1][1]*v.y + m[1][2]*v.z,
+        m[2][0]*v.x + m[2][1]*v.y + m[2][2]*v.z
+        );
+}
+Mat3 Mat3::operator*(const Mat3& rhs) const {
+    Mat3 result;
+    for(int i=0;i<3; i++){
+        for(int j=0;j<3; j++){
+            result.m[i][j]=0.0f;
+            for(int k=0; k<3; ++k) {
+                result.m[i][j]+=m[i][k]*rhs.m[k][j];
+            }
+        }
+    }
+    return result;
 }
